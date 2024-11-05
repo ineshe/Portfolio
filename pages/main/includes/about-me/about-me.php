0728d236ed8ba75df54e913a5a6bff1a79222c70
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2024-11-05 14:58:40 +0100

    Change profile picture

--- pages/main/includes/about-me/about-me.php
+++ pages/main/includes/about-me/about-me.php
@@ -1,6 +1,6 @@
 <article id="about-me" class="section">
     <div class="content">
-        <img class="picture" src="<?php echo $baseURL . '/data/profilbild.jpg'?>" alt="">
+        <img class="picture" src="<?php echo $baseURL . '/data/profilbild.webp'?>" alt="">
         <p class="text">Hallo, ich bin Ines und beschäftige mich seit meinem Studium der Medieninformatik mit Webentwicklung. Durch meine Ausbildung zur gestaltungstechnischen Assistentin verfüge ich neben dem technischen Know&#8209;how auch über ein Gespür für gutes Design.</p>
         <!--<p class="text">Guten Tag! Mein Name ist Ines und ich beschäftige mich seit meinem Abschluss im Fach &bdquo;Medieninformatik&ldquo; an der Hochschule Mittweida mit der Entwicklung von Webanwendungen. Dank meiner Ausbildung als &bdquo;gestaltungstechnische Assistentin&ldquo; bringe ich dafür neben dem technischen Know&#8209;how auch ein Gespür für gutes Design mit. Das Lösen von Programmierproblemen gehört so wie das Lösen von Kreuzworträtseln in meiner Freizeit zu meinen Leidenschaften.</p> -->
     </div>
