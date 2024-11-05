feda4add9707a535bfc029cd2f0d06c1a992c4d7
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2024-11-05 14:59:42 +0100

    Change script order

--- pages/main/includes/cookie-consent/cookie-consent.js
+++ pages/main/includes/cookie-consent/cookie-consent.js
@@ -13,18 +13,17 @@ function loadGoogleAnalytics() {
     var script1 = document.createElement('script');
     script1.src = "https://www.googletagmanager.com/gtag/js?id=G-Y4LB5LLVVS";
     script1.async = true;
-    document.head.appendChild(script1);
 
-    script1.onload = function() {
-        var script2 = document.createElement('script');
-        script2.innerHTML = `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-Y4LB5LLVVS');
-        `;
-        document.head.appendChild(script2);
-    };
+    var script2 = document.createElement('script');
+    script2.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-Y4LB5LLVVS', {'anonymize_ip': true});
+    `;
+
+    document.head.prepend(script2);
+    document.head.prepend(script1);
 }
 
 window.onload = function() {
