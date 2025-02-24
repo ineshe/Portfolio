8a2212f5a532d3345ac0fba655a00e84d0af6bb4
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2025-02-24 13:23:58 +0100

    Change text

--- src/views/partials/about-me/about-me.php
+++ src/views/partials/about-me/about-me.php
@@ -1,6 +1,6 @@
 <article id="about-me" class="section">
     <div class="content">
         <img class="picture" src="<?php echo $baseURL?>/assets/images/profilbild.webp" alt="">
-        <p class="text">Hallo, ich bin Ines. Ich entwickle und optimiere Webshops und Webseiten – mit Erfahrung in Shopware, Symfony und anderen modernen Webtechnologien. Durch mein Know-how in der Webentwicklung und mein Gespür für Design entstehen Weblösungen, die sowohl technisch als auch optisch überzeugen.</p>
+        <p class="text">Hallo, ich bin Ines. Ich entwickle und optimiere Websites und Shops – mit Erfahrung in Shopware, Symfony und anderen modernen Technologien. Durch mein Know-how in der Webentwicklung und mein Gespür für Design entstehen Lösungen, die sowohl technisch als auch optisch überzeugen.</p>
     </div>
 </article>
