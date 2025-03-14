4381b323465d4094ebc5b34f11820bf22e0206ec
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2025-03-14 15:12:26 +0100

    Refactor footer styling and add social media links

--- src/views/partials/footer/footer.php
+++ src/views/partials/footer/footer.php
@@ -1,4 +1,12 @@
 <footer>
-    <a href="<?php echo $baseURL . '/impressum'?>">Impressum</a>
-    <a href="<?php echo $baseURL . '/datenschutz'?>">Datenschutzerklärung</a>
+    <div class="content">
+        <ul class="links">
+            <li><a href="<?php echo $baseURL . '/impressum'?>">Impressum</a></li>
+            <li><a href="<?php echo $baseURL . '/datenschutz'?>">Datenschutzerklärung</a></li>            
+        </ul>
+        <div class="social-links">
+            <a class="social-link" href="https://github.com/ineshe" target="_blank"><img class="social-icon" src="<?php echo $baseURL?>/assets/icons/github.svg"></a>
+            <a class="social-link" href="https://www.linkedin.com/in/ines-heilmann-5350891a8" target="_blank"><img class="social-icon" src="<?php echo $baseURL?>/assets/icons/linkedin.svg"></a>
+        </div>
+    </div>
 </footer>
