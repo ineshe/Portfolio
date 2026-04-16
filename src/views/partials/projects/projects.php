8c5e0d571b42fc8be3c6b4831fbf2495b331febc
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2026-04-16 18:55:19 +0200

    Refactor project display structure for improved clarity and consistency

--- src/views/partials/projects/projects.php
+++ src/views/partials/projects/projects.php
@@ -6,37 +6,33 @@
     <div class="content">
         <h2 class="section-title">Projekte</h2>
         <div class="project-cards">
-
-        <?php 
-            if(isset($projects)):
-                foreach ($projects as $project):
-                    if ($project->visibility == "1"): 
-        ?>
-
-            <section class="project">
-                <a href="<?= $baseURL . '/project/' . $project->slug ?>">
-                    <img class="card-img" src="<?= $baseURL . $project->mainImage ?>" alt="">
+            <?php 
+                if(isset($projects)):
+                    foreach ($projects as $project):
+                        if ($project->visibility == "1"): 
+            ?>
+                <div class="project">
+                    <a href="<?= $baseURL . '/project/' . $project->slug ?>">
+                        <img class="card-img" src="<?= $baseURL . $project->mainImage ?>" alt="">
+                        <div class="card-text">
+                            <h3 class="project-title"><?= $project->title ?></h3>
+                            <p><?= $project->technologies ?></p>
+                        </div>
+                    </a>
+                </div>
+            <?php 
+                        endif;
+                    endforeach;
+                endif;
+            ?>
+            <div class="project">
+                <a href="<?php echo $baseURL?>/more-projects">
+                    <img class="card-img" src="<?php echo $baseURL?>/assets/projects/more-projects.png" alt="Weiter">
                     <div class="card-text">
-                        <h3 class="project-title"><?= $project->title ?></h3>
-                        <p><?= $project->technologies ?></p>
+                        <h3 class="project-title">Mehr Projekte ansehen</h3>
                     </div>
                 </a>
-            </section>
-
-        <?php 
-                    endif;
-                endforeach;
-            endif;
-        ?>
-
-        <section class="project">
-            <a href="<?php echo $baseURL?>/more-projects">
-                <img class="card-img" src="<?php echo $baseURL?>/assets/projects/more-projects.png" alt="Weiter">
-                <div class="card-text">
-                    <h3 class="project-title">Mehr Projekte ansehen</h3>
-                </div>
-            </a>
-        </section>
+            </div>
         </div> 
     </div>
 </section>
