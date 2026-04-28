ca21231744f6c725d73facf09cda387aab46428f
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2026-04-28 15:47:23 +0200

    Swap button positions

--- src/views/components/cookie-consent/cookie-consent.php
+++ src/views/components/cookie-consent/cookie-consent.php
@@ -1,7 +1,7 @@
 <div id="cookie-banner">
     <p>Diese Website verwendet Cookies, um Analysedaten mit Hilfe von Google Analytics zu erfassen. Weitere Informationen finden Sie in der <a href="https://www.ines-heilmann.de/pages/datenschutz/datenschutz.php">Datenschutzerklärung</a>.</p>
     <div class="buttons">
-        <button onclick="acceptCookies()">Zustimmen</button>
         <button onclick="declineCookies()">Ablehnen</button>
+        <button onclick="acceptCookies()">Zustimmen</button>
     </div>
 </div>
